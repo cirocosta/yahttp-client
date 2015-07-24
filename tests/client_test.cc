2a30486cdf5745f8d518aeaba0c4b2c435684fb2
Ciro S. Costa <ciro9758@gmail.com>
2015-07-24 16:49:35 -0300

    adds test for generating GETs for different path

--- tests/client_test.cc
+++ tests/client_test.cc
@@ -5,7 +5,7 @@
 using namespace yahttp;
 using namespace yahttp::client;
 
-TEST(Client, SimpleLocalhost) {
+TEST(Client, GetSamePath) {
   const char* url_location = "http://localhost:8080/path";
   const char* expected_get =
     "GET /path HTTP/1.1\r\n"
@@ -20,3 +20,17 @@ TEST(Client, SimpleLocalhost) {
   EXPECT_EQ(expected_get, req_msg);
 }
 
+TEST(Client, GetDifferentPath) {
+  const char* url_location = "http://localhost:8080/photos/my_face.png";
+  const char* expected_get =
+    "GET /photos/my_face.png HTTP/1.1\r\n"
+    "Host: localhost:8080\r\n"
+    "\r\n";
+
+  URL url (url_location);
+  Client client (url);
+
+  std::string req_msg = client.gen_get("/photos/my_face.png");
+
+  EXPECT_EQ(expected_get, req_msg);
+}
